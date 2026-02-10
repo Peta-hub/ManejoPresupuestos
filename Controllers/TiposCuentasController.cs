b3ebffd3cef555af1235f7effa2511b0450689c5
peter <pmonroylara@gmail.com>
2026-02-10 16:11:11 -0600

    move table items

--- Controllers/TiposCuentasController.cs
+++ Controllers/TiposCuentasController.cs
@@ -128,5 +128,11 @@ namespace ManejoPresupuestos.Controllers
             return Json(true);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            return Ok();
+        }
+
     }
 }
