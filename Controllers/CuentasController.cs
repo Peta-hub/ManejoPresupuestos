9fe611b996a59021129b48b60466eefb6fa4007e
peter <pmonroylara@gmail.com>
2026-02-10 18:41:45 -0600

    init cuentas

--- Controllers/CuentasController.cs
+++ Controllers/CuentasController.cs
@@ -1,7 +1,28 @@
+using ManejoPresupuestos.Models;
+using ManejoPresupuestos.Servicios;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace ManejoPresupuestos.Controllers;
 
 public class CuentasController: Controller
 {
-    
+    private readonly IReposotorioTiposCuentas reposotorioTiposCuentas;
+    private readonly IServicioUsuarios servicioUsuarios;
+
+    public CuentasController(IReposotorioTiposCuentas reposotorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+    {
+        this.reposotorioTiposCuentas = reposotorioTiposCuentas;
+        this.servicioUsuarios = servicioUsuarios;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Crear()
+    {
+        var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+        var tiposCuentas = await reposotorioTiposCuentas.Obtener(usuarioId);
+        var modelo = new CuentaCreacionViewModel();
+        modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+
+        return View(modelo);
+    }
 }
